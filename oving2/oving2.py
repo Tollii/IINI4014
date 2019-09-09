6387cfaf21065133d1008361c15edd1245c5b332
Tollii <andreas.tolnes@ymail.com>
2019-09-10 01:26:05 +0200

    Øving 2

---
+++ oving2/oving2.py
@@ -0,0 +1,24 @@
+import math
+
+def archimedesPI(iterations = 10):
+    """Returns an estimate of Pi calculated with Archimedes' method"""
+    if iterations >= 501:
+        print("Choose a lower number")
+        return None
+
+    lengthOfSide = 1
+    numberOfSides = 6
+
+    for x in range(iterations):
+        halfSide = lengthOfSide / 2
+        a = math.sqrt(1 - (halfSide ** 2))
+        b = 1 - a
+        newSide = math.sqrt((b**2) + (halfSide**2))
+        perimiter = numberOfSides* lengthOfSide
+        pi = perimiter / 2
+        lengthOfSide = newSide
+        numberOfSides *= 2
+    return pi
+
+print(archimedesPI(500))
+
